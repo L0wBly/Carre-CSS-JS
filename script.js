0e6c311b6348756731dccfe8f6e13309794c5b0d
Arthur <arthurboutry2005@icloud.com>
2024-11-05 22:15:47 +0100

    ajout test bouton cliquer

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let bouton = document.getElementById("bouton");
+let result = document.getElementById("cliquer")
 let Bcolor = bouton.style.backgroundColor;
         function change() {
             if ( bouton.style.backgroundColor == Bcolor) {
                 bouton.style.backgroundColor = "red";
+                result.innerHTML = "Background Color changed";
             }
             else {
                 bouton.style.backgroundColor = Bcolor
+                result.innerHTML = "";
             }
         }  
+
